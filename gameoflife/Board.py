bfe21b1c37659304c17086f26803f50bdcba4a68
Justin Carruthers <Jtcruthers@gmail.com>
2018-07-14 00:31:54 -0400

    Using ratio for seeding

--- gameoflife/Board.py
+++ gameoflife/Board.py
@@ -8,13 +8,13 @@ class Board():
         self.board = [[" "for x in range(width)] for x in range(height)]
 
 
-    def seed(self):
+    def seed(self, ratio=0.15):
         total_cells = self.height * self.width
-        number_of_seeds = math.floor(total_cells * 0.1)
-        for _ in range(number_of_seeds):
+        number_of_seeds = math.floor(total_cells * ratio)
+        for x in range(number_of_seeds):
             seed_index = (random.randint(0,self.height - 1), random.randint(0,self.width - 1))
             self.board[seed_index[0]][seed_index[1]] = "X"
-
+            print(seed_index)
 
     def print(self):
         for _ in range(50):
