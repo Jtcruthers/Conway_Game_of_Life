8dd1277ed1afa7e5a397b802107caa9182506151
Justin Carruthers <Jtcruthers@gmail.com>
2018-07-15 01:01:47 -0400

    Basic impolementation

--- gameoflife/Board.py
+++ gameoflife/Board.py
@@ -2,19 +2,26 @@ import math, random
 
 class Board():
 
+    ALIVE = "X"
+    DEAD = " "
+
     def __init__(self, height=10, width=10):
         self.height = height
         self.width = width
-        self.board = [[" "for x in range(width)] for x in range(height)]
+        self.board = [[self.DEAD for x in range(width)] for x in range(height)]
 
 
     def seed(self, ratio=0.15):
+        """
+            Seeds the board with random live cells. Can give ratio of live to total cells
+        """
+
         total_cells = self.height * self.width
         number_of_seeds = math.floor(total_cells * ratio)
         for x in range(number_of_seeds):
             seed_index = (random.randint(0,self.height - 1), random.randint(0,self.width - 1))
-            self.board[seed_index[0]][seed_index[1]] = "X"
-            print(seed_index)
+            self.board[seed_index[0]][seed_index[1]] = self.ALIVE
+
 
     def print(self):
         for _ in range(50):
@@ -25,3 +32,31 @@ class Board():
         for _ in range(50):
             print("-", end="")
         print()
+
+
+    def get_surrounding_live_cell_count(self, row, column):
+        count = 0
+        count += 1 if self.is_cell_alive(row - 1, column - 1) else 0
+        count += 1 if self.is_cell_alive(row - 1, column) else 0
+        count += 1 if self.is_cell_alive(row - 1, column + 1) else 0
+        count += 1 if self.is_cell_alive(row, column - 1) else 0
+        count += 1 if self.is_cell_alive(row, column + 1) else 0
+        count += 1 if self.is_cell_alive(row + 1, column - 1) else 0
+        count += 1 if self.is_cell_alive(row + 1, column) else 0
+        count += 1 if self.is_cell_alive(row + 1, column + 1) else 0
+        return count
+
+
+    def is_cell_alive(self, row, column):
+        try:
+            return self.board[row][column] == self.ALIVE
+        except IndexError:
+            return False
+
+
+    def kill_cell(self, row, column):
+        self.board[row][column] = self.DEAD
+
+
+    def awaken_cell(self, row, column):
+        self.board[row][column] = self.ALIVE
--- gameoflife/game_of_life.py
+++ gameoflife/game_of_life.py
@@ -1,10 +1,14 @@
 from gameoflife.Board import Board
 import copy
 
+NUM_OF_SIMS = 10
+
 def run():
     board = create_board(10, 10)
-    run_simulation(board)
     print_board(board)
+    for _ in range(NUM_OF_SIMS):
+        board = run_simulation(board)
+        print_board(board)
 
 
 def create_board(height, width):
@@ -22,7 +26,17 @@ def run_simulation(board):
         Runs the entire simulation on a given board
     """
 
-    old_board = copy.deepcopy(board)
+    new_board = copy.deepcopy(board)
+    for row in range(board.height):
+        for column in range(board.width):
+            number_of_neighbors_alive = board.get_surrounding_live_cell_count(row, column)
+
+            if number_of_neighbors_alive < 2 or number_of_neighbors_alive > 3:
+                new_board.kill_cell(row, column)
+            else:
+                if not board.is_cell_alive(row, column) and number_of_neighbors_alive == 3:
+                    new_board.awaken_cell(row, column)
+    return new_board
 
 
 def print_board(board):
